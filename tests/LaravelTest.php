3c2e52c0db1d07b3cbcd78e18a18249e792505fc
Andrew Kane <andrew@ankane.org>
2022-09-10 00:38:48 -0700

    Improved Laravel tests

--- tests/LaravelTest.php
+++ tests/LaravelTest.php
@@ -5,42 +5,60 @@ use PHPUnit\Framework\TestCase;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Database\Eloquent\Model;
 
+$capsule = new Capsule();
+$capsule->addConnection([
+    'driver' => 'pgsql',
+    'database' => 'pgvector_php_test'
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+Pgvector\Laravel\Schema::register();
+
+Capsule::statement('CREATE EXTENSION IF NOT EXISTS vector');
+Capsule::schema()->dropIfExists('items');
+Capsule::schema()->create('items', function ($table) {
+    $table->increments('id');
+    $table->vector('factors', 3);
+});
+
 class Item extends Model
 {
     public $timestamps = false;
-    protected $fillable = ['factors'];
+    protected $fillable = ['id', 'factors'];
 }
 
 final class LaravelTest extends TestCase
 {
-    public function testWorks()
+    public function setUp(): void
     {
-        $capsule = new Capsule();
-        $capsule->addConnection([
-            'driver' => 'pgsql',
-            'database' => 'pgvector_php_test'
-        ]);
-        $capsule->setAsGlobal();
-        $capsule->bootEloquent();
-
-        Pgvector\Laravel\Schema::register();
-
-        Capsule::schema()->dropIfExists('items');
-        Capsule::schema()->create('items', function ($table) {
-            $table->increments('id');
-            $table->vector('factors', 3);
-        });
-
-        foreach (['[1,1,1]', '[2,2,2]', '[1,1,2]'] as $v) {
-            Item::create(['factors' => $v]);
+        Item::truncate();
+        foreach (['[1,1,1]', '[2,2,2]', '[1,1,2]'] as $i => $v) {
+            Item::create(['id' => $i + 1, 'factors' => $v]);
         }
+    }
 
+    public function testL2Distance()
+    {
         $neighbors = Item::orderByRaw('factors <-> ?', ['[1,1,1]'])->take(5)->get();
-        $ids = array_map(fn ($v) => $v['id'], $neighbors->toArray());
-        $factors = array_map(fn ($v) => $v['factors'], $neighbors->toArray());
-        $this->assertEquals([1, 3, 2], $ids);
-        $this->assertEquals(['[1,1,1]', '[1,1,2]', '[2,2,2]'], $factors);
+        $this->assertEquals([1, 3, 2], $neighbors->pluck('id')->toArray());
+        $this->assertEquals(['[1,1,1]', '[1,1,2]', '[2,2,2]'], $neighbors->pluck('factors')->toArray());
+    }
 
+    public function testMaxInnerProduct()
+    {
+        $neighbors = Item::orderByRaw('factors <#> ?', ['[1,1,1]'])->take(5)->get();
+        $this->assertEquals([2, 3, 1], $neighbors->pluck('id')->toArray());
+    }
+
+    public function testCosineDistance()
+    {
+        $neighbors = Item::orderByRaw('factors <=> ?', ['[1,1,1]'])->take(5)->get();
+        $this->assertEquals([1, 2, 3], $neighbors->pluck('id')->toArray());
+    }
+
+    public function testDistances()
+    {
         $distances = Item::selectRaw('factors <-> ? AS distance', ['[1,1,1]'])->pluck('distance');
         $this->assertEqualsWithDelta([0, sqrt(3), 1], $distances->toArray(), 0.00001);
     }
