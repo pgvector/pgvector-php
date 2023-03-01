fb71135c667e9c730de3de57d35d491815f8cf20
Andrew Kane <andrew@ankane.org>
2023-02-28 19:35:39 -0800

    Set max depth and throw on error

--- src/laravel/Vector.php
+++ src/laravel/Vector.php
@@ -10,7 +10,7 @@ class Vector implements CastsAttributes
     public function get(Model $model, string $key, mixed $value, array $attributes): ?array
     {
         if (!is_null($value)) {
-            return json_decode($value, true);
+            return json_decode($value, true, 2, JSON_THROW_ON_ERROR);
         }
         return null;
     }
@@ -18,7 +18,7 @@ class Vector implements CastsAttributes
     public function set(Model $model, string $key, mixed $value, array $attributes): ?string
     {
         if (is_array($value)) {
-            return json_encode($value);
+            return json_encode($value, JSON_THROW_ON_ERROR, 1);
         }
         return $value;
     }
