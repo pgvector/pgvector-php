fe34c18608e6ba07b6224e977e7719e30d0b009b
Andrew Kane <andrew@ankane.org>
2023-02-28 19:46:41 -0800

    Use Castable

--- src/laravel/Vector.php
+++ src/laravel/Vector.php
@@ -2,26 +2,32 @@
 
 namespace Pgvector\Laravel;
 
+use Illuminate\Contracts\Database\Eloquent\Castable;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Database\Eloquent\Model;
 
-class Vector implements CastsAttributes
+class Vector implements Castable
 {
-    public function get(Model $model, string $key, mixed $value, array $attributes): ?array
+    public static function castUsing(array $arguments): CastsAttributes
     {
-        if (!is_null($value)) {
-            $decoded = json_decode($value, true, 2, JSON_THROW_ON_ERROR);
-            return is_array($decoded) ? $decoded : null;
-        }
-        return null;
-    }
+        return new class () implements CastsAttributes {
+            public function get(Model $model, string $key, mixed $value, array $attributes): ?array
+            {
+                if (!is_null($value)) {
+                    $decoded = json_decode($value, true, 2, JSON_THROW_ON_ERROR);
+                    return is_array($decoded) ? $decoded : null;
+                }
+                return null;
+            }
 
-    public function set(Model $model, string $key, mixed $value, array $attributes): ?string
-    {
-        if (is_array($value)) {
-            return json_encode($value, JSON_THROW_ON_ERROR, 1);
-        }
-        // TODO throw error?
-        return null;
+            public function set(Model $model, string $key, mixed $value, array $attributes): ?string
+            {
+                if (is_array($value)) {
+                    return json_encode($value, JSON_THROW_ON_ERROR, 1);
+                }
+                // TODO throw error?
+                return null;
+            }
+        };
     }
 }
