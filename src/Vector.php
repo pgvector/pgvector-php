85ad2c61795a9a8f8a918b5b7c08575f839bbdcf
Andrew Kane <andrew@ankane.org>
2023-03-11 16:00:36 -0800

    Added todo [skip ci]

--- src/Vector.php
+++ src/Vector.php
@@ -20,6 +20,7 @@ class Vector
             }
         }
 
+        // TODO use array_is_list when PHP < 8.1 no longer supported
         if (!is_array($value)) {
             throw new \InvalidArgumentException("Expected array");
         }
