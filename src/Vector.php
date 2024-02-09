85c1f5a97b8d15fc8cf47b3a8625bdd85728b754
fuwasegu <admin@fuwasegu.com>
2024-02-09 12:35:50 +0900

    refactor: 💡 add strict mode

--- src/Vector.php
+++ src/Vector.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Pgvector;
 
 class Vector
--- src/laravel/PgvectorServiceProvider.php
+++ src/laravel/PgvectorServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Pgvector\Laravel;
 
 use Illuminate\Support\ServiceProvider;
