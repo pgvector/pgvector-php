549a239ce97f105351396c709e18094ad96c0b39
Andrew Kane <andrew@ankane.org>
2024-06-24 20:21:55 -0700

    Added more tests for SparseVector [skip ci]

--- tests/SparseVectorTest.php
+++ tests/SparseVectorTest.php
@@ -6,13 +6,26 @@ use Pgvector\SparseVector;
 
 final class SparseVectorTest extends TestCase
 {
+    public function testFromDense()
+    {
+        $embedding = SparseVector::fromDense([1, 0, 2, 0, 3, 0]);
+        $this->assertEquals(6, $embedding->dimensions());
+        $this->assertEquals([0, 2, 4], $embedding->indices());
+        $this->assertEquals([1, 2, 3], $embedding->values());
+    }
+
+    public function testFromString()
+    {
+        $embedding = SparseVector::fromString('{1:1,3:2,5:3}/6');
+        $this->assertEquals(6, $embedding->dimensions());
+        $this->assertEquals([0, 2, 4], $embedding->indices());
+        $this->assertEquals([1, 2, 3], $embedding->values());
+    }
+
     public function testToString()
     {
         $embedding = SparseVector::fromDense([1, 0, 2, 0, 3, 0]);
         $this->assertEquals('{1:1,3:2,5:3}/6', (string) $embedding);
-        $this->assertEquals(6, $embedding->dimensions());
-        $this->assertEquals([0, 2, 4], $embedding->indices());
-        $this->assertEquals([1, 2, 3], $embedding->values());
     }
 
     public function testToArray()
